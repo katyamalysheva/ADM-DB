6b42ffc8b8ff28fe00f0f09e9770b754b411d282
katyamalysheva <katyamalysheva2000@yandex.ru>
2024-06-06 22:00:09 +0300

    Registration-receipts tables created

--- sql/create.sql
+++ sql/create.sql
@@ -56,3 +56,50 @@ CREATE TABLE IF NOT EXISTS Salon (
     closing_time TIME NOT NULL,
     opening_time TIME NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Client (
+    client_id INTEGER PRIMARY KEY,
+    last_name VARCHAR(40) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    middle_name VARCHAR(40),
+    gender CHAR(1) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    birth_date DATE,
+    email VARCHAR(40)
+);
+
+CREATE TABLE IF NOT EXISTS Registration (
+    registration_id INTEGER PRIMARY KEY,
+    client_id INTEGER NOT NULL,
+    date DATE NOT NULL,
+    closing_time TIME NOT NULL,
+    opening_time TIME NOT NULL,
+    master_id INTEGER NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES Client (client_id),
+    FOREIGN KEY (master_id) REFERENCES Master (master_id)
+);
+
+CREATE TABLE IF NOT EXISTS Service_Registration (
+    service_id INTEGER NOT NULL,
+    registration_id INTEGER NOT NULL,
+    PRIMARY KEY (service_id, registration_id),
+    FOREIGN KEY (service_id) REFERENCES Service (service_id),
+    FOREIGN KEY (registration_id) REFERENCES Registration (registration_id)
+);
+
+CREATE TABLE IF NOT EXISTS Promotion (
+    promotion_id INTEGER PRIMARY KEY,
+    discount INTEGER NOT NULL,
+    promotion_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Receipt (
+    receipt_id INTEGER PRIMARY KEY,
+    registration_id INTEGER NOT NULL,
+    promotion_id INTEGER NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    total_sum DECIMAL(8, 2) NOT NULL,
+    FOREIGN KEY (registration_id) REFERENCES Registration (registration_id),
+    FOREIGN KEY (promotion_id) REFERENCES Promotion (promotion_id)
+);
